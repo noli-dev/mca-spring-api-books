f9a51d916ab2a88278dc82d05065d4dc8fbb7583
manu <manloralm@outlook.com>
2023-06-05 20:31:31 +0200

    Add line breaker flag

---
+++ src/main/java/es/urjc/code/daw/library/toggle/Features.java
@@ -0,0 +1,11 @@
+package es.urjc.code.daw.library.toggle;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.Label;
+
+public enum Features implements Feature{
+    
+    @Label("Activates line breaker")
+    LINE_BREAKER
+
+}
